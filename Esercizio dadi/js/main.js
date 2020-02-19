82f25c26567f1f27b1ccbaa2e536005cbf9650c7
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-02-19 21:14:23 +0100

    aggiunto il css al secondo esercizio

--- Esercizio dadi/js/main.js
+++ Esercizio dadi/js/main.js
@@ -3,15 +3,20 @@
 //Per prima cosa uso due variabili che fungono da dadi usando random
 //Dopo dobbiamo determinare quale dei due numeri sia maggiore
 
-var dadoUno = Math.floor(Math.random() * 6)+1;
+var dadoUno = Math.floor(Math.random() * 6) +1;
     console.log(dadoUno);
-var dadoDue = Math.floor(Math.random() * 6)+1;
+    document.getElementById('primo-dado').innerHTML = 'Il primo dado ha come risultato: ' + dadoUno;
+var dadoDue = Math.floor(Math.random() * 6) +1;
     console.log(dadoDue);
+    document.getElementById('secondo-dado').innerHTML = 'Il secondo dado ha come risultato: ' + dadoDue;
 
 if (dadoUno > dadoDue) {
-    console.log('vince il primo dado')
+    console.log('vince il primo dado');
+    document.getElementById('esito').innerHTML = 'Vince il primo dado'
 } else if (dadoUno == dadoDue) {
-    console.log('pareggio, tirate di nuovo')
+    console.log('pareggio, tirate di nuovo');
+    document.getElementById('esito').innerHTML = 'Pareggio, tirate di nuovo'
 } else {
-    console.log('vince il secondo dado')
+    console.log('vince il secondo dado');
+    document.getElementById('esito').innerHTML = 'Vince il secondo dado'
 }
